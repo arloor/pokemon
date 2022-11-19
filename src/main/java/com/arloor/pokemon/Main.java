a1bd700000dd0d9d50195c6a5bb5b59e0c695a1c
arloor <admin@arloor.com>
2022-11-20 02:43:10 +0800

    增加太晶

--- src/main/java/com/arloor/pokemon/Main.java
+++ src/main/java/com/arloor/pokemon/Main.java
@@ -5,6 +5,7 @@ import com.google.common.collect.Sets;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
 public class Main {
@@ -22,61 +23,81 @@ public class Main {
                 String[] split = line.split("[ ,]");
                 if (split.length <= 1) {
                     System.out.println("命令长度太短");
+                    continue;
                 }
                 List<String> params = new ArrayList<>();
                 for (int i = 1; i < split.length; i++) {
                     params.add(split[i]);
                 }
-                if (TEAM.equals(split[0])){
+                if (TEAM.equals(split[0])) {
                     team(params);
-                }
-                if (ATTACK.equals(split[0])){
+                } else if (ATTACK.equals(split[0])) {
                     attack(params);
+                } else {
+                    System.out.println("命令错误");
                 }
-                System.out.println();
             } catch (Throwable e) {
                 e.printStackTrace();
             }
-        }while (true);
+        } while (true);
 
     }
 
     private static void attack(List<String> params) {
-        Set<String> duplicates = new HashSet<>();
-        boolean needShowDuplicates=false;
-        for (String param : params) {
-           Set<String> elements=Domain.弱点.getOrDefault(param,new HashSet<>());
-            System.out.println(param+" 的弱点有："+String.join(",", elements));
-            if (duplicates.size()==0){
-                duplicates.addAll(elements);
-            }else {
-                needShowDuplicates=true;
-                duplicates.retainAll(elements);
+        Map<String, AtomicInteger> cells = new HashMap<>();
+        List<String> toAttack = new ArrayList<>(params);
+        List<String> toFangshou = new ArrayList<>(params);
+        for (String param : toAttack) {
+            if (param.startsWith("太晶")) {
+                param = param.substring(2);
+                cells.clear();
+                Set<String> elements = Domain.弱点.getOrDefault(param, new HashSet<>());
+                for (String element : elements) {
+                    cells.computeIfAbsent(element, k -> new AtomicInteger(0)).addAndGet(2);
+                }
+                break;
+            } else {
+                Set<String> elements = Domain.弱点.getOrDefault(param, new HashSet<>());
+                for (String element : elements) {
+                    cells.computeIfAbsent(element, k -> new AtomicInteger(0)).addAndGet(2);
+                }
             }
         }
-        if (needShowDuplicates&&duplicates.size()!=0){
-            System.out.println("多倍弱点有："+String.join(",", duplicates));
+        for (String s : toFangshou) {
+            if (s.startsWith("太晶")) {
+                s = s.substring(2);
+            }
+            Set<String> needDelete = Domain.优势.getOrDefault(s, new HashSet<>());
+            for (String s1 : needDelete) {
+                cells.remove(s1);
+            }
         }
+        System.out.println("可以选用：" + cells.entrySet().stream().sorted(new Comparator<Map.Entry<String, AtomicInteger>>() {
+            @Override
+            public int compare(Map.Entry<String, AtomicInteger> o1, Map.Entry<String, AtomicInteger> o2) {
+                return o2.getValue().get()-o1.getValue().get();
+            }
+        }).map(entry -> entry.getKey() + ":" + entry.getValue()).collect(Collectors.joining(",")));
     }
 
     private static void team(List<String> params) {
-        Set<String> result=new HashSet<>();
+        Set<String> result = new HashSet<>();
         for (String param : params) {
-            result.addAll(Domain.优势.getOrDefault(param,new HashSet<>()));
+            result.addAll(Domain.优势.getOrDefault(param, new HashSet<>()));
         }
-        System.out.println("该队伍对以下属性效果绝佳："+ String.join(",", result));
+        System.out.println("该队伍对以下属性效果绝佳：" + String.join(",", result));
         Set<String> strings = Domain.getAll();
         strings.removeAll(result);
-        System.out.println("盲点属性有："+String.join(",", strings));
+        System.out.println("盲点属性有：" + String.join(",", strings));
     }
 
-    public static final void hint(){
+    public static final void hint() {
         System.out.println("请输入命令：");
     }
 
     public static final void printUsage() {
         System.out.println("Usage:");
-        System.out.println("1. 队伍 草 钢 岩石 水");
-        System.out.println("2. 攻击 草 钢");
+        System.out.println("1.队伍 虫 水 幽灵 毒 草 飞行 一般 妖精 格斗");
+        System.out.println("2.攻击 岩石 太晶草");
     }
 }
